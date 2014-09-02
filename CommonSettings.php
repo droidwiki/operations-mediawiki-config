8ff3476e226975ffb70bb857133f073f2428e8aa
Florianschmidtwelzow <florian.schmidt.welzow@t-online.de>
2014-09-02 14:29:24 +0200

    Move favicon to images/
    So no need to look at single files in wikiroot before upgrade, just
    copy the folders.
    
    Change-Id: Id549b990c105079d66de6e24f7c348d3a1f1ae94

--- CommonSettings.php
+++ CommonSettings.php
@@ -37,7 +37,7 @@
 	## The relative URL path to the logo.  Make sure you change this from the default,
 	## or else you'll overwrite your logo when you upgrade!
 	$wgLogo = 'androide.png';
-	$wgFavicon = 'favicon.ico';
+	$wgFavicon = 'images/favicon.ico';
 
 	## UPO means: this is also a user preference option
 	$wgEnableEmail = true;
