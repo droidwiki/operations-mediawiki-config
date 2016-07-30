58c854cd6f0f93d84e7b7f3b6a62f3686b21ef63
Florian <florian.schmidt.welzow@t-online.de>
2016-07-30 20:45:41 +0200

    Add a hhvm error document
    This document will be delivered to the user, when hhvm experienced a fatal
    error. The configuration is set in hhvm as:
    
    Change-Id: Idceb897dffbd9ddcced7162f8d6844027bcf31f8

---
+++ docroot/hhvm-fatal-error.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!-- taken from WMF: https://goo.gl/TFaF97 -->
+<html lang=en>
+<meta charset="utf-8">
+<title>DroidWiki Error</title>
+<link rel="shortcut icon" href="//www.droidwiki.de/images/favicon.ico">
+<style>
+* { margin: 0; padding: 0; }
+div.body { background: #fff; margin: 7% auto 0; padding: 2em 1em 1em; font: 14px/21px sans-serif; color: #333; max-width: 560px; }
+img { float: left; margin: 0 2em 2em 0; }
+a img { border: 0; }
+h1 { margin-top: 1em; font-size: 1.2em; }
+p { margin: 0.7em 0 1em 0; }
+a { color: #0645AD; text-decoration: none; }
+a:hover { text-decoration: underline; }
+em { font-style: normal; color: #777; }
+div.footer { text-align: center; position: absolute; background: rgba(245, 245, 245, 1); width: 100%; bottom: 0; height: 10em; border-top: 1px solid lightgray; color: #777; }
+</style>
+<div class="body">
+	<a href="//www.droidwiki.de"><img src="//www.droidwiki.de/images/androide.png" alt=DroidWiki width=135 height=135></a>
+	<h1>DroidWiki Error</h1>
+	<p>Our servers are currently experiencing a technical problem. This is probably temporary and should be fixed soon. Please <a href="javascript:" onclick="window.location.reload(false); return false">try again</a> in a few minutes.<br><em>That’s all we know.</em></p>
+</div>
+<div class="footer">
+	PHP fatal error<?php
+	// Guard against "Cannot modify header information - headers already sent" warning
+	if ( !headers_sent() ) {
+		header( 'HTTP/1.1 500 Internal Server Error' );
+	}
+        $err = error_get_last();
+        $message = $err['message'];
+        # error_get_last() doesn't return a fully populated array in HHVM,
+        # capture file and line manually
+        if ( preg_match( '/#0\\s+(\\S+?)\\((\\d+)\\)/', $message, $matches ) ) {
+		echo ' ' . htmlspecialchars( $matches[1] ) . ' line ' . $matches[2];
+	}
+	$parts = explode( "\n", $message );
+	$message = $parts[0];
+	$message = preg_replace( "/^.*?exception '.*?' with message '(.*?)'.*$/im", '\1', $message );
+	// Increment a counter.
+	$sock = socket_create( AF_INET, SOCK_DGRAM, SOL_UDP );
+	$stat = 'MediaWiki.errors.fatal:1|c';
+	socket_sendto( $sock, $stat, strlen( $stat ), 0, '188.68.49.74', 8125 );
+        ?>: <br/>
+        <?php echo htmlspecialchars( $message ); ?>
+</div>
+</html>
