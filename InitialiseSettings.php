d0c7123f668ea953329ad82890218299272c3db2
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2017-01-13 04:31:36 +0100

    Revert "Change logo to christmas version"
    This reverts commit fcc50b3d6afc64f1e0248a34bb7b93770d872554.
    
    Change-Id: I4a29a38ee26cd8e893916fdf1bfc276922d7dc63

--- InitialiseSettings.php
+++ InitialiseSettings.php
@@ -33,8 +33,8 @@ $wgConf->settings = [
 		// androide.png -> blank DroidWiki logo
 		// androide_ch.png -> DroidWiki logo with christmas hat
 		'default' => '',
-		'droidwiki' => '/static/images/project-logos/androide_ch.png',
-		'datawiki' => '/static/images/project-logos/androide_ch.png',
+		'droidwiki' => '/static/images/project-logos/androide.png',
+		'datawiki' => '/static/images/project-logos/androide.png',
 	],
 
 	'wgFavicon' => [
