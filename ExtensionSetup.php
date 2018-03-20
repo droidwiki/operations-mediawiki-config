80fb26fa28628b9f8f65fb9a6754ae3746e74a8f
Florian <florian.schmidt.welzow@t-online.de>
2018-03-20 22:38:41 +0100

    Remove need to check extension existence on the fly and remove not needed/loaded extensions
    Change-Id: Ib8c548b2bdc18ca141bf2ae021955d406f1c8683

--- ExtensionSetup.php
+++ ExtensionSetup.php
@@ -1,13 +1,12 @@
 <?php
 # DO NOT PUT PRIVATE INFORMATION HERE!
 
-$extToLoad = [
+$jsonEntrypointExtensions = [
 	'AmazonPartnernet',
 	'AntiSpoof', # needed by AbuseFilter
 	'Cite', # ref-tags
 	'ParserFunctions',
 	'SyntaxHighlight_GeSHi',
-	'CanonURL',
 	'InputBox',
 	'OpenGraphMeta',
 	'MultimediaViewer',
@@ -15,23 +14,18 @@ $extToLoad = [
 	'CookieWarning',
 	'EmbedVideo', # Allows to embed YouTube videos into wikipages
 	'Disambiguator',
-	'DynamicPageList',
 	'Echo',
 	'BetaFeatures',
 	'PageImages', # PageImages (needed by MobileFrontend and HoverCards)
 	'TextExtracts', # TextExtracts (needed by MobileFrontend and HoverCards)
 	'Popups',
-	'ExpandTamplates',
 	'MwEmbedSupport',
-	'TimedMediaHandler',
 	'ConfirmEdit',
 	'AbuseFilter',
 	'StopForumSpam',
 	'Elastica',
-	'CirrusSearch',
 	'WikiEditor',
 	'CodeEditor',
-	'googleAnalytics',
 	'MobileFrontend',
 	'Scribunto',
 	'Thanks',
@@ -46,9 +40,7 @@ $extToLoad = [
 	'Citoid',
 	'Interwiki',
 	'TemplateSandbox',
-	'LdapAuthentication',
 	'UniversalLanguageSelector',
-	'Translate',
 	'GoogleSiteLinksSearchBox',
 	'OATHAuth',
 	'GeoData',
@@ -59,243 +51,195 @@ $extToLoad = [
 	'Nuke',
 ];
 
-$extensionsToLoadWithExtensionregistration = [];
-foreach ( $extToLoad as $name ) {
+$phpEntrypointExtensions = [
+	'googleAnalytics',
+	'CanonURL',
+	'TimedMediaHandler',
+	'Translate',
+	'CirrusSearch',
+	'LdapAuthentication',
+];
+
+function shouldSkipLoadExtension( $name ) {
 	$useExtensionConfigName = 'wmgUse' . $name;
-	if ( isset( $$useExtensionConfigName ) && !$$useExtensionConfigName ) {
+	return isset( $$useExtensionConfigName ) && !$$useExtensionConfigName;
+}
+
+foreach ( $phpEntrypointExtensions as $name ) {
+	if ( shouldSkipLoadExtension( $name ) ) {
 		continue;
 	}
+	require "$IP/extensions/$name/$name.php";
+}
 
-	$extensionInformation = wfGetExtensionInformation( $name );
-	if ( $extensionInformation['exists'] ) {
-		switch( $extensionInformation['installType'] ) {
-			case 'json':
-				$extensionsToLoadWithExtensionregistration[] = $name;
-				break;
-			default:
-				require_once "$IP/extensions/$name/$name.php";
-		}
+$extensionsToLoadWithExtensionregistration = [];
+foreach ( $jsonEntrypointExtensions as $name ) {
+	if ( shouldSkipLoadExtension( $name ) ) {
+		continue;
 	}
+	$extensionsToLoadWithExtensionregistration[] = $name;
 }
 
 wfLoadExtensions( $extensionsToLoadWithExtensionregistration );
 
-function wfExtensionExists( $name ) {
-	return wfGetExtensionInformation( $name )['exists'];
-}
-
-function wfGetExtensionInformation( $name ) {
-	global $IP;
-
-	$retval = [
-		'exists' => false,
-		'installType' => null,
-	];
-	if ( file_exists( "$IP/extensions/$name/$name.php" ) ) {
-		$retval['exists'] = true;
-		$retval['installType'] = 'php';
-	}
-	if ( file_exists( "$IP/extensions/$name/extension.json" ) ) {
-		$retval['exists'] = true;
-		$retval['installType'] = 'json';
-	}
-	return  $retval;
-}
-
 # Configuration for ConfirmEdit
-if ( wfExtensionExists( "ConfirmEdit" ) ) {
-	if ( PHP_SAPI === 'cli' ) {
-		$wgMessagesDirs['ReCaptchaNoCaptcha'] = "$IP/extensions/ConfirmEdit/ReCaptchaNoCaptcha/i18n";
-		$wgMessagesDirs['FancyCaptcha'] = "$IP/extensions/ConfirmEdit/FancyCaptcha/i18n";
-	}
-	// The DroidWiki app can't handle client side JavaScript (on which reCaptcha is based on)
-	// Check, if the request is made via the api (and assume, that this is the app or any other client that
-	// needs machine readable format's) and use the FancyCaptcha plugin instead of reCaptcha.
-	if ( $_SERVER['SCRIPT_NAME'] !== '/api.php' && isset( $_GET['title'] ) && strpos( $_GET['title'], 'Captcha/image' ) === false ) {
-		require_once "$IP/extensions/ConfirmEdit/ReCaptchaNoCaptcha.php";
-		$wgReCaptchaSiteKey = $wmgReCaptchaSiteKey;
-		$wgReCaptchaSecretKey = $wmgReCaptchaSecretKey;
-		$wgCaptchaClass = 'ReCaptchaNoCaptcha';
-	} else {
-		require_once "$IP/extensions/ConfirmEdit/FancyCaptcha.php";
-		$wgCaptchaDirectory = $wmgFancyCaptchaCaptchaDir;
-		$wgCaptchaSecret = $wmgFancyCaptchaSecretKey;
-		$wgCaptchaClass = 'FancyCaptcha';
-		// in order to work with clients other then web browsers, the CAPTCHA information needs to be
-		// stored in th cache, instead of in the session (which is the default).
-		$wgCaptchaStorageClass = 'CaptchaCacheStore';
-	}
-
-	# only emailconfirmed can skip captcha
-	$wgGroupPermissions['*']['skipcaptcha'] = false;
-	$wgGroupPermissions['user']['skipcaptcha'] = false;
-	$wgGroupPermissions['autoconfirmed']['skipcaptcha'] = false;
-	$wgGroupPermissions['bot']['skipcaptcha'] = false;
-	$wgGroupPermissions['sysop']['skipcaptcha'] = false;
-	$wgGroupPermissions['emailconfirmed']['skipcaptcha'] = true;
-	$ceAllowConfirmedEmail = true;
-
-	# Trigger for ConfirmEdit
-	$wgCaptchaTriggers['edit'] = true;
-	$wgCaptchaTriggers['create'] = true;
-	$wgCaptchaTriggers['addurl'] = true;
-	$wgCaptchaTriggers['createaccount'] = true;
-	$wgCaptchaTriggers['badlogin'] = true;
+if ( PHP_SAPI === 'cli' ) {
+	$wgMessagesDirs['ReCaptchaNoCaptcha'] = "$IP/extensions/ConfirmEdit/ReCaptchaNoCaptcha/i18n";
+	$wgMessagesDirs['FancyCaptcha'] = "$IP/extensions/ConfirmEdit/FancyCaptcha/i18n";
 }
+
+// The DroidWiki app can't handle client side JavaScript (on which reCaptcha is based on)
+// Check, if the request is made via the api (and assume, that this is the app or any other client that
+// needs machine readable format's) and use the FancyCaptcha plugin instead of reCaptcha.
+if ( $_SERVER['SCRIPT_NAME'] !== '/api.php' && isset( $_GET['title'] ) && strpos( $_GET['title'], 'Captcha/image' ) === false ) {
+	require_once "$IP/extensions/ConfirmEdit/ReCaptchaNoCaptcha.php";
+	$wgReCaptchaSiteKey = $wmgReCaptchaSiteKey;
+	$wgReCaptchaSecretKey = $wmgReCaptchaSecretKey;
+	$wgCaptchaClass = 'ReCaptchaNoCaptcha';
+} else {
+	require_once "$IP/extensions/ConfirmEdit/FancyCaptcha.php";
+	$wgCaptchaDirectory = $wmgFancyCaptchaCaptchaDir;
+	$wgCaptchaSecret = $wmgFancyCaptchaSecretKey;
+	$wgCaptchaClass = 'FancyCaptcha';
+	// in order to work with clients other then web browsers, the CAPTCHA information needs to be
+	// stored in th cache, instead of in the session (which is the default).
+	$wgCaptchaStorageClass = 'CaptchaCacheStore';
+}
+
+# only emailconfirmed can skip captcha
+$wgGroupPermissions['*']['skipcaptcha'] = false;
+$wgGroupPermissions['user']['skipcaptcha'] = false;
+$wgGroupPermissions['autoconfirmed']['skipcaptcha'] = false;
+$wgGroupPermissions['bot']['skipcaptcha'] = false;
+$wgGroupPermissions['sysop']['skipcaptcha'] = false;
+$wgGroupPermissions['emailconfirmed']['skipcaptcha'] = true;
+$ceAllowConfirmedEmail = true;
+
+# Trigger for ConfirmEdit
+$wgCaptchaTriggers['edit'] = true;
+$wgCaptchaTriggers['create'] = true;
+$wgCaptchaTriggers['addurl'] = true;
+$wgCaptchaTriggers['createaccount'] = true;
+$wgCaptchaTriggers['badlogin'] = true;
 
 # AbuseFilter
-if ( wfExtensionExists( "AbuseFilter" ) ) {
-	$wgGroupPermissions['sysop']['abusefilter-modify'] = true;
-	$wgGroupPermissions['*']['abusefilter-log-detail'] = true;
-	$wgGroupPermissions['*']['abusefilter-view'] = true;
-	$wgGroupPermissions['*']['abusefilter-log'] = true;
-	$wgGroupPermissions['sysop']['abusefilter-private'] = true;
-	$wgGroupPermissions['sysop']['abusefilter-modify-restricted'] = true;
-	$wgGroupPermissions['sysop']['abusefilter-revert'] = true;
-}
+$wgGroupPermissions['sysop']['abusefilter-modify'] = true;
+$wgGroupPermissions['*']['abusefilter-log-detail'] = true;
+$wgGroupPermissions['*']['abusefilter-view'] = true;
+$wgGroupPermissions['*']['abusefilter-log'] = true;
+$wgGroupPermissions['sysop']['abusefilter-private'] = true;
+$wgGroupPermissions['sysop']['abusefilter-modify-restricted'] = true;
+$wgGroupPermissions['sysop']['abusefilter-revert'] = true;
 
 # Stop Forum Spam
-if ( wfExtensionExists( "StopForumSpam" ) ) {
-	$wgSFSAPIKey = $wmgSFSAPIKey;
-	$wgPutIPinRC = true;
-}
+$wgSFSAPIKey = $wmgSFSAPIKey;
+$wgPutIPinRC = true;
 
 # Elasticsearch
-if ( wfExtensionExists( "Elastica" ) && wfExtensionExists( "CirrusSearch" ) ) {
-	$wgSearchType = 'CirrusSearch';
-	$wgCirrusSearchPowerSpecialRandom = $wmgCirrusSearchPowerSpecialRandom;
-	$wgCirrusSearchServers = [ '188.68.49.74' ];
-}
+$wgSearchType = 'CirrusSearch';
+$wgCirrusSearchPowerSpecialRandom = $wmgCirrusSearchPowerSpecialRandom;
+$wgCirrusSearchServers = [ '188.68.49.74' ];
 
 # WikiEditor/graphical Editor
-if ( wfExtensionExists( "WikiEditor" ) ) {
-	$wgDefaultUserOptions['usebetatoolbar'] = 1;
-	$wgDefaultUserOptions['usebetatoolbar-cgd'] = 1;
-	$wgDefaultUserOptions['wikieditor-preview'] = 0;
-}
+$wgDefaultUserOptions['usebetatoolbar'] = 1;
+$wgDefaultUserOptions['usebetatoolbar-cgd'] = 1;
+$wgDefaultUserOptions['wikieditor-preview'] = 0;
 
 # CodeEditor (extension for WikiEditor
-if ( wfExtensionExists( "CodeEditor" ) ) {
-	# Enable it on JS/CSS pages
-	$wgCodeEditorEnableCore = true;
-}
+# Enable it on JS/CSS pages
+$wgCodeEditorEnableCore = true;
 
 # Add Google-Analytics
-if ( wfExtensionExists( "googleAnalytics" ) ) {
-	$wgGoogleAnalyticsAccount = $wmgGoogleAnalyticsAccount;
-	$wgGoogleAnalyticsIgnoreSysops = $wmgGoogleAnalyticsIgnoreSysops;
-	$wgGoogleAnalyticsIgnoreBots = $wmgGoogleAnalyticsIgnoreBots;
-}
+$wgGoogleAnalyticsAccount = $wmgGoogleAnalyticsAccount;
+$wgGoogleAnalyticsIgnoreSysops = $wmgGoogleAnalyticsIgnoreSysops;
+$wgGoogleAnalyticsIgnoreBots = $wmgGoogleAnalyticsIgnoreBots;
 
 # MobileFrontend
-if ( wfExtensionExists( "MobileFrontend" ) ) {
-	wfLoadSkin( 'MinervaNeue' );
-	$wgMobileFrontendLogo = "{$wgScriptPath}/static/images/project-logos/androide.png";
-	$wgMFAutodetectMobileView = true;
-	$wgMFEnableBeta = true;
-	$wgMFSpecialCaseMainPage = true;
-	$wgMFAllowNonJavaScriptEditing = true;
+wfLoadSkin( 'MinervaNeue' );
+$wgMobileFrontendLogo = "{$wgScriptPath}/static/images/project-logos/androide.png";
+$wgMFAutodetectMobileView = true;
+$wgMFEnableBeta = true;
+$wgMFSpecialCaseMainPage = true;
+$wgMFAllowNonJavaScriptEditing = true;
 
-	if ( $wmgUseWikibaseClient ) {
-		$wgMFUseWikibaseDescription = true;
-		$wgMFDisplayWikibaseDescription = true;
-	}
+if ( $wmgUseWikibaseClient ) {
+	$wgMFUseWikibaseDescription = true;
+	$wgMFDisplayWikibaseDescription = true;
 }
 
 # Scribunto
-if ( wfExtensionExists( "Scribunto" ) ) {
-	$wgScribuntoDefaultEngine = 'luastandalone';
-	$wgScribuntoUseGeSHi = true;
-	$wgScribuntoUseCodeEditor = true;
-}
+$wgScribuntoDefaultEngine = 'luastandalone';
+$wgScribuntoUseGeSHi = true;
+$wgScribuntoUseCodeEditor = true;
 
-if ( $wmgUseDroidWiki && wfExtensionExists( "DroidWiki" ) ) {
+if ( $wmgUseDroidWiki ) {
 	require_once "$IP/extensions/DroidWiki/DroidWiki.php";
 	$wgDroidWikiAdDisallowedNamespaces = [ 120, 121, 122, 123 ];
 }
 
 # Thanks
-if ( wfExtensionExists( "Thanks" ) ) {
-	$wgIncludejQueryMigrate = true;
-	$wgThanksConfirmationRequired = true;
-}
+$wgIncludejQueryMigrate = true;
+$wgThanksConfirmationRequired = true;
 
 # GoogleLogin
-if ( wfExtensionExists( "GoogleLogin" ) ) {
-	$wgGLSecret = $wmgGLSecret;
-	$wgGLAppId = $wmgGLAppId;
-	$wgGLAPIKey = $wmgGLAPIKey;
-	$wgGLShowCreateReason = true;
-	$wgGLShowRight = true;
-	// FIXME: reset these two to default value, after mw-ui buttons fixed in HTMLForm
-	$wgGLShowKeepLogin = false;
-	$wgGLForceKeepLogin = true;
-	$wgGLAllowAccountCreation = true;
-}
+$wgGLSecret = $wmgGLSecret;
+$wgGLAppId = $wmgGLAppId;
+$wgGLAPIKey = $wmgGLAPIKey;
+$wgGLShowCreateReason = true;
+$wgGLShowRight = true;
+// FIXME: reset these two to default value, after mw-ui buttons fixed in HTMLForm
+$wgGLShowKeepLogin = false;
+$wgGLForceKeepLogin = true;
+$wgGLAllowAccountCreation = true;
+
 
 # CentralNotice - 01.08.2014
-if ( wfExtensionExists( "CentralNotice" ) ) {
-	$wgCentralDBname = 'droidwikiwiki';
-	if ( $multiversion->getDBName() === $wgCentralDBname ) {
-		$wgNoticeInfrastructure = true;
-	}
-	$wgCentralPagePath = "//www.droidwiki.org/w/index.php";
-	$wgCentralSelectedBannerDispatcher = "//www.droidwiki.org/w/index.php?title=Special:BannerLoader";
-	$wgNoticeProjects = [ 'droidwikiwiki', 'datawiki' ];
-	$wgNoticeProject = $multiversion->getWikiName();
+$wgCentralDBname = 'droidwikiwiki';
+if ( $multiversion->getDBName() === $wgCentralDBname ) {
+	$wgNoticeInfrastructure = true;
 }
+$wgCentralPagePath = "//www.droidwiki.org/w/index.php";
+$wgCentralSelectedBannerDispatcher = "//www.droidwiki.org/w/index.php?title=Special:BannerLoader";
+$wgNoticeProjects = [ 'droidwikiwiki', 'datawiki' ];
+$wgNoticeProject = $multiversion->getWikiName();
 
-if ( wfExtensionExists ( "GoogleAnalyticsTopPages" ) ) {
-	$wgGATPProfileId = $wmgGATPProfileId;
-	$wgGATPKeyFileLocation = $wmgGATPKeyFileLocation;
-	$wgGATPServiceAccountName = $wmgGATPServiceAccountName;
-}
+$wgGATPProfileId = $wmgGATPProfileId;
+$wgGATPKeyFileLocation = $wmgGATPKeyFileLocation;
+$wgGATPServiceAccountName = $wmgGATPServiceAccountName;
 
 # TemplateData
-if ( wfExtensionExists( "TemplateData" ) ) {
-	$wgTemplateDataUseGUI = true;
-}
+$wgTemplateDataUseGUI = true;
 
 # VisualEditor
-if ( wfExtensionExists ( "VisualEditor" ) ) {
-	$wgDefaultUserOptions['visualeditor-enable'] = 1;
-	$wgVisualEditorSupportedSkins = [ 'vector', 'apex', 'monobook', 'minerva' ];
-	$wgVisualEditorEnableWikitext = true;
-	$wgVisualEditorEnableDiffPage = true;
-	$wgVisualEditorAvailableNamespaces = [
-		NS_TALK => true,
-		NS_USER => true,
-		NS_USER_TALK => true,
-		NS_PROJECT => true,
-		NS_PROJECT_TALK => true,
-		NS_FILE => true,
-		NS_FILE_TALK => true,
-		NS_HELP => true,
-		NS_HELP_TALK => true,
-		NS_CATEGORY => true,
-		NS_CATEGORY_TALK => true,
-	];
-}
+$wgDefaultUserOptions['visualeditor-enable'] = 1;
+$wgVisualEditorSupportedSkins = [ 'vector', 'apex', 'monobook', 'minerva' ];
+$wgVisualEditorEnableWikitext = true;
+$wgVisualEditorEnableDiffPage = true;
+$wgVisualEditorAvailableNamespaces = [
+	NS_TALK => true,
+	NS_USER => true,
+	NS_USER_TALK => true,
+	NS_PROJECT => true,
+	NS_PROJECT_TALK => true,
+	NS_FILE => true,
+	NS_FILE_TALK => true,
+	NS_HELP => true,
+	NS_HELP_TALK => true,
+	NS_CATEGORY => true,
+	NS_CATEGORY_TALK => true,
+];
 
-if ( wfExtensionExists ( 'CookieWarning' ) ) {
-	$wgCookieWarningEnabled = true;
-	$wgCookieWarningMoreUrl =
-		'https://www.droidwiki.org/DroidWiki:Impressum#Verwendung_von_Cookies_.28Cookie-Policy.29';
-}
+$wgCookieWarningEnabled = true;
+$wgCookieWarningMoreUrl =
+	'https://www.droidwiki.org/DroidWiki:Impressum#Verwendung_von_Cookies_.28Cookie-Policy.29';
 
-if ( wfExtensionExists ( 'UserMerge' ) ) {
-	$wgGroupPermissions['bureaucrat']['usermerge'] = true;
-}
+$wgGroupPermissions['bureaucrat']['usermerge'] = true;
 
-if ( wfExtensionExists( 'Citoid' ) ) {
-	$wgCitoidServiceUrl = 'https://go2tech.de/citoid/api';
-}
+$wgCitoidServiceUrl = 'https://go2tech.de/citoid/api';
 
-if ( wfExtensionExists( 'Interwiki' ) ) {
-	$wgGroupPermissions['sysop']['interwiki'] = true;
-}
+$wgGroupPermissions['sysop']['interwiki'] = true;
 
-if ( $wmgUseLdapAuthentication && wfExtensionExists( 'LdapAuthentication' ) ) {
+if ( $wmgUseLdapAuthentication ) {
 	$wgAuth = new LdapAuthenticationPlugin();
 	$wgLDAPDomainNames = [
 		'go2tech.de',
@@ -347,7 +291,7 @@ if ( $wmgUseLdapAuthentication && wfExtensionExists( 'LdapAuthentication' ) ) {
 		'go2tech.de' => true,
 	];
 }
-if ( $wmgUseTranslate && wfExtensionExists( 'Translate' ) ) {
+if ( $wmgUseTranslate ) {
 	$wgTranslateTranslationServices = [
 		'Yandexm' => [
 			'key' => $wmgTranslateTranslationServicesKeys['Yandex'],
@@ -361,9 +305,7 @@ if ( $wmgUseTranslate && wfExtensionExists( 'Translate' ) ) {
 	];
 }
 
-if ( wfExtensionExists( 'TemplateSandbox' ) ) {
-	$wgTemplateSandboxEditNamespaces[] = 828;
-}
+$wgTemplateSandboxEditNamespaces[] = 828;
 
 $wmgWikibaseBaseNs = 120;
 // Define custom namespaces. Use these exact constant names.
@@ -476,15 +418,13 @@ if ( $wmgUseWikibaseClient ) {
 	};
 }
 
-if ( $wmgUseOATHAuth && wfExtensionExists( 'OATHAuth' ) ) {
+if ( $wmgUseOATHAuth ) {
 	$wgSharedTables[] = 'oathauth_users';
 }
 
-if ( wfExtensionExists( 'GeoData' ) ) {
-	$wgGeoDataBackend = 'elastic';
-}
+$wgGeoDataBackend = 'elastic';
 
-if ( $wmgUseXenForoAuth && wfExtensionExists( 'XenForoAuth' ) ) {
+if ( $wmgUseXenForoAuth ) {
 	$wgXenForoAuthBaseUrl = 'http://android-hilfe.de/api/';
 	$wgXenForoAuthClientId = 'ca9why0jle';
 	$wgXenForoAuthClientSecret = $wmgXenForoAuthClientSecret;
@@ -492,7 +432,7 @@ if ( $wmgUseXenForoAuth && wfExtensionExists( 'XenForoAuth' ) ) {
 	$wgXenForoAuthAutoCreate = true;
 }
 
-if ( $wmgUseContentTranslation && wfExtensionExists( 'ContentTranslation' ) ) {
+if ( $wmgUseContentTranslation ) {
 	$wgContentTranslationRESTBase = [
 		'url' => 'https://www.droidwiki.org/api/v1',
 		'fixedUrl' => true,
@@ -516,6 +456,5 @@ if ( $wmgUseContentTranslation && wfExtensionExists( 'ContentTranslation' ) ) {
 	];
 }
 
-if ( wfExtensionExists( 'GlobalUsage' ) ) {
-	$wgGlobalUsageDatabase = 'droidwikiwiki';
-}
+$wgGlobalUsageDatabase = 'droidwikiwiki';
+
