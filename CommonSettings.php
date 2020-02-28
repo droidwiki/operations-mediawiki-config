d70aceda7b7b75ab272a2ac7936ab08f82f55097
Florian <florian.schmidt.welzow@t-online.de>
2020-02-28 19:56:54 +0100

    Purge all cache servers

--- CommonSettings.php
+++ CommonSettings.php
@@ -193,7 +193,7 @@ if ( $wmgUseInstantCommons ) {
 if ( $wmgUseVarnish ) {
 	$wgUseCdn = true;
 	$wgCdnServersNoPurge = [ '172.16.0.1/16', '10.0.0.0/16' ];
-	$wgCdnServers = [ '172.16.0.1:6081' ];
+	$wgCdnServers = [ 'donut.dwnet:6081', 'eclair.dwnet:6081' ];
 	$wgUsePrivateIPs = true;
 }
 
