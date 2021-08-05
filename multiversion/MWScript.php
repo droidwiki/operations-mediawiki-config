489fda2b073fef05b3d86480b97c6b398a4107a3
Florian <florian.schmidt.welzow@t-online.de>
2021-08-05 20:22:21 +0200

    Docker contexts might run as root

--- multiversion/MWScript.php
+++ multiversion/MWScript.php
@@ -25,28 +25,6 @@ function getMWScriptWithArgs() {
 		exit( 1 );
 	}
 
-	# Security check -- don't allow scripts to run as privileged users
-	$gids = posix_getgroups();
-	foreach ( $gids as $gid ) {
-		$info = posix_getgrgid( $gid );
-		if ( $info && in_array( $info['name'], [ 'wikidev', 'root' ] ) ) {
-			fwrite( STDERR,
-				"Cannot run a MediaWiki script as a user in the " . "group {$info['name']}\n" );
-			fwrite( STDERR, <<<EOT
-Maintenance scripts should generally be run using sudo -u apache which
-is available to all wikidev users.  Running a maintenance script as a
-privileged user risks compromise of the user account.
-
-You should run this script as the apache user:
-
- sudo -u apache <command>
-
-EOT
-			);
-			exit( 1 );
-		}
-	}
-
 	$relFile = $argv[1]; // the script file to run
 	# If no MW directory is given then assume this is a /maintenance script
 	if ( strpos( $relFile, '/' ) === false ) {
