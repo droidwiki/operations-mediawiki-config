fea6a3663687d0cc6bd829a61900fb6b1f65fc60
Florian <florian.schmidt.welzow@t-online.de>
2022-01-29 12:50:43 +0100

    fix codestyle

--- CommonSettings.php
+++ CommonSettings.php
@@ -205,7 +205,7 @@ if ( $wmgUseVarnish ) {
 }
 
 if ( isset( $_SERVER['HTTP_X_DEBUG_SERVER'] ) ) {
-	var_dump('X-Forwarded-For: ', $_SERVER['HTTP_X_FORWARDED_FOR']);
+	var_dump( 'X-Forwarded-For: ', $_SERVER['HTTP_X_FORWARDED_FOR'] );
 }
 
 require_once __DIR__ . '/ExtensionSetup.php';
